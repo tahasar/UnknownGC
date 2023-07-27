80d4884a6fec2a8aa1ee8be99f53f3c55c46a4f6
Serhat Kıldacı <96617749+serhatkildaci@users.noreply.github.com>
2023-07-27 14:48:43 +0300

    JSON Parse System

---
+++ Assets/Scripts/JSONReader.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JSONReader : MonoBehaviour
+{
+    public TextAsset textJSON;
+    
+    [System.Serializable]
+
+    public class Player{
+        public string name;
+        public int health;
+        public int damage;
+    }
+
+    [System.Serializable]
+    public class PlayerList{
+        public Player[] player;
+    }
+
+    public PlayerList myPlayerList = new PlayerList();
+
+    void Start()
+    {
+        myPlayerList = JsonUtility.FromJson<PlayerList>(textJSON.text);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
